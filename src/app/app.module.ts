0e9826b167b0a887268b3da42e7df55801e22480
Alex Manuel <al3x.mg@gmail.com>
2023-06-14 12:27:26 -0600

    Added MaterialUI and created first component for images

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ImageGalleryComponent } from './gallery/image-gallery/image-gallery.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImageGalleryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
